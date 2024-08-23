31cbcd2cb464d009ef45891afa5d61545af46395
kunsel-c-at-school <kunseljc26@ausdk12.org>
2024-08-23 19:39:32 +0000

    noice

--- Card.java
+++ Card.java
@@ -1,3 +1,26 @@
-public class Card {
+public class Card{
+    private int suit;
+    private int rank;
     
+    //Constructor
+    public Card(int suit, int rank){
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    //Set
+    public void setRank(int rank){
+        this.rank = rank;
+    }
+    public void setSuit(int suit){
+        this.suit = suit;
+    }
+    
+    //Return functions
+    public int suit(){
+        return suit;
+    }
+    public int rank(){
+        return rank;
+    }
 }
--- CardMain.java
+++ CardMain.java
@@ -1,5 +1,7 @@
 public class CardMain {
     public static void main(String[] args) {
-        
+        Card c = new Card(1, 2);
+        Card c1 = new Card(3, 11);
+        Card c2 = new Card(4, 10);
     }
 }
